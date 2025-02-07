53ab4f44dcaf26b27f88ee7cbaabbe6d0f12f253
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-07 20:28:49 -0300

    exercicios 8 e 9

--- exercicio8.py
+++ exercicio8.py
@@ -1,3 +1,4 @@
-y = int(input("Digite dois numero inteiros: "))
-resultado = input + input
+num1 = int(input("Digite um numero inteiro: "))
+num2 = int(input("Digite um numero inteiro: "))
+resultado =  num1 + num2
 print("O total é: ", resultado)
---
+++ exercicio9.py
@@ -0,0 +1,5 @@
+num1 = int (input("Digite um número inteiro: "))
+num2 = int (input("Digite um número inteiro: "))
+num3 = int (input("Digite um número inteiro: "))
+resultado = num1 + num2 * num3
+print("O total é: ", resultado)
