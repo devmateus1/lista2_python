91dafc3814a2ec2139e0f4ba8fd2204a6421ccd7
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-07 20:37:17 -0300

    exercicio10

---
+++ exercicio10.py
@@ -0,0 +1,4 @@
+qntdfatia = int(input("Quantos pedaços o bolo tem ?"))
+pedacoscomidos = int(input("Quantos pedaços você comeu ?"))
+pedacosrestantes = qntdfatia - pedacoscomidos
+print("Sobraram :", pedacosrestantes)
